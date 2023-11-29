6ee4f16ebb11b3255d07fd5d11195a8c1287470b
SofiaDanali <148966939+SofiaDanali@users.noreply.github.com>
2023-11-29 13:00:10 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+import gf.Questionnaire;
+import gf.TestAPI;
+
+public class Main {
+
+        public static void main(String[] args) {
+            TestAPI.setApiKey("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");         
+            Questionnaire questionnaire = new Questionnaire();
+            List<String> userResponses = questionnaire.collectUserResponses();
+            String request = "Find 5 brands of wines that are: " + userResponses.get(1) + "and " + userResponses.get(3)
+                 + ". Please display them strictly like this: name - color and flavor";
+           
+            // Print user responses (you can modify this part based on your needs)
+            System.out.println("User Responses: " + userResponses);
+           
+            // Use GPT-3.5 to analyze responses and get wine recommendations
+            try {
+                ArrayList<String> gptResponse = TestAPI.getWineRecommendations(request);
+
+                // Display the recommended wines to the user
+                System.out.println("Recommended Wines:");
+                System.out.println(gptResponse);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } 
+    }
