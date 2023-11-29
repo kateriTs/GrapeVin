39cb34eb2bcc794fc48e88cb16caa55dd5f1978c
SofiaDanali <148966939+SofiaDanali@users.noreply.github.com>
2023-11-29 12:57:06 +0200

    Update TestAPI.java

--- TestAPI.java
+++ TestAPI.java
@@ -7,6 +7,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class TestAPI {
     private static String API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
@@ -20,7 +21,7 @@ public class TestAPI {
         return apiKey;
     }
 
-    public static String getWineRecommendations(String[] userResponses) throws Exception {
+    public static ArrayList<String> getWineRecommendations(String request) throws Exception {
 
         HttpClient httpClient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(API_ENDPOINT);
@@ -28,7 +29,7 @@ public class TestAPI {
         // Create JSON payload for GPT-3.5 API request
         String jsonPayload = "{ \"model\": \"gpt-3.5-turbo-1106\", \"messages\": ["
         + "{\"role\": \"system\", \"content\": \"You are a helpful assistant.\"},"
-        + "{\"role\": \"user\", \"content\": \"Find 5 wines that are: " + userResponses[0] + ", " + userResponses[1] + " and " + userResponses[2] + "\"}"
+        + "{\"role\": \"user\", \"content\": \"Find 5 wines that are: " + request + "\"}"
         + "] }";
 
         // Set headers
@@ -50,6 +51,25 @@ public class TestAPI {
             result.append(line);
         }
 
-        return result.toString();
+        return parseGptResponse(result.toString());
+    }
+
+      private static ArrayList<String> parseGptResponse(String gptResponse) {
+        // Parse the JSON response to extract the recommended wines
+        ArrayList<String> recommendedWines = new ArrayList<>();
+
+        // Extract the assistant's content from the JSON response
+        String content = gptResponse.split("\"content\": \"")[1].split("\"")[0];
+
+        // Split the content into lines
+        String[] lines = content.split("\\\\n");
+
+        // Add each line to the ArrayList
+        for (String line : lines) {
+            recommendedWines.add(line.trim());
+        }
+
+        return recommendedWines;
     }
 }
+
