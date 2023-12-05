ae0df3148a2288440d923ee8389bce2da452d3e8
SofiaDanali <148966939+SofiaDanali@users.noreply.github.com>
2023-12-05 20:43:52 +0200

    Update Main.java
    Εχω βαλει τους πίνακες για τα κρασιά και τη μεθοδο για την εισαγωγή των κρασιών στους πίνακες στη main για λόγους αμεσότητας. Μπορουμε να το αλλάξουμε μετα αλλα θεωρώ πως τωρα είναι πιο πρακτικό απο το να έχουμε κλαση για τα κρασιά διότι δεν δημηουργούμε αντικείμενα κρασιών. 
    
    το πρόβλημα που προκείπτει συμβαίνει στη μέθοδο saveWinesInArrays, οπου δεν μπαίνουν τα κρασιά.

--- GrapeVin/GrapeVin/Main.java
+++ GrapeVin/GrapeVin/Main.java
@@ -4,6 +4,9 @@ import gf.Questionnaire;
 import gf.TestAPI;
 
 public class Main {
+        private static ArrayList<String> whiteWines;
+        private static ArrayList<String> redWines;
+        private static ArrayList<String> roseWines;
 
         public static void main(String[] args) {         
             Questionnaire questionnaire = new Questionnaire();
@@ -17,12 +20,31 @@ public class Main {
             // Use GPT-3.5 to analyze responses and get wine recommendations
             try {
                 ArrayList<String> gptResponse = TestAPI.getWineRecommendations(request);
+                saveWinesInArrays(gptResponse);
 
                 // Display the recommended wines to the user
                 System.out.println("Recommended Wines:");
                 System.out.println(gptResponse);
+                // Display the content of the ArrayLists
+                System.out.println(whiteWines);
+                System.out.println(redWines);
+                System.out.println(roseWines);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } 
+        }
+        // Put the wines of the gpt response in the ArrayLists
+         public static void saveWinesInArrays(ArrayList<String> gptWineList) {
+            for (String wine : gptWineList) {
+                if (wine.toLowerCase().contains("white".toLowerCase())){
+                    whiteWines.add(wine);
+                } else if (wine.toLowerCase().contains("red".toLowerCase())){
+                    redWines.add(wine);
+                } else if (wine.toLowerCase().contains("rose".toLowerCase())){
+                    roseWines.add(wine);
+                } else {
+                    System.out.println("Unknown Wine Category");
+                }
+            }
+        }
 }
