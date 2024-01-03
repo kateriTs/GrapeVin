69e09e5fb58d6f5c474b80d1f8b3c5886b8155b7
SofiaDanali <148966939+SofiaDanali@users.noreply.github.com>
2024-01-03 12:32:14 +0200

    Add files via upload

---
+++ GrapeVin/GrapeVin/src/main/gf/java/SQLConnection.java
@@ -0,0 +1,22 @@
+package gf;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLConnection {
+
+    public static Connection getConnection() throws ClassNotFoundException {
+        Connection connection = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/winedb?user=root&password=Foolcavetown201:3306/winedb";
+            String username = "root";
+            String password = "Foolcavetown201";
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the database!");
+        } catch (SQLException e) {
+            System.err.println("Connection error: " + e.getMessage());
+        }
+        return connection;
+    }
+}
