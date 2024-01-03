e93149bda529ca3c90b6f837af8d7e7831a8bc4e
SofiaDanali <148966939+SofiaDanali@users.noreply.github.com>
2024-01-03 13:11:18 +0200

    Update SQLConnection.java

--- GrapeVin/GrapeVin/src/main/gf/java/SQLConnection.java
+++ GrapeVin/GrapeVin/src/main/gf/java/SQLConnection.java
@@ -11,7 +11,7 @@ public class SQLConnection {
         try {
             String url = "jdbc:mysql://localhost:3306/winedb?user=root&password=Foolcavetown201:3306/winedb";
             String username = "root";
-            String password = "Foolcavetown201";
+            String password = "putyourpassword";
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the database!");
         } catch (SQLException e) {
