2225f9330c66af459c5e18aca179ac14b149565d
SofiaDanali <148966939+SofiaDanali@users.noreply.github.com>
2024-01-09 20:16:45 +0200

    Update winedbase.sql

--- GrapeVin/GrapeVin/src/main/gf/winedbase.sql
+++ GrapeVin/GrapeVin/src/main/gf/winedbase.sql
@@ -1,4 +1,18 @@
-hUSE winedb;
+USE winedb;
+CREATE TABLE users (
+	username VARCHAR(8) NOT NULL UNIQUE,
+	user_password VARCHAR(8) NOT NULL,
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE QuizAnswers (
+	username VARCHAR(8) NOT NULL,
+	w_name1 VARCHAR(45) NOT NULL,
+	w_name2 VARCHAR(45) NOT NULL,
+	w_name3 VARCHAR(45) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
 CREATE TABLE WhiteWines (
 	w_name VARCHAR(45) NOT NULL UNIQUE,
 	w_color VARCHAR(45) NOT NULL,
